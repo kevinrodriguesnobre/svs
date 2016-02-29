2f732266f89821f933d8441ccfab0d5a53df3cea
Guncraker <guncraker@hotmail.de>
2016-02-29 12:44:13 +0100

    PlayerDatei und Linkdatei für Tutorials

---
+++ core/src/com/kyte/svs/Player.java
@@ -0,0 +1,19 @@
+package com.kyte.svs;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Created by Yanneck on 29.02.2016.
+ *
+ * Stellt den Spieler dar, enthält sämtliche Eigenschaften,
+ * wie Geschwindigkeit, mit der sich der Spieler bewegen soll,
+ * o.ä..
+ *
+ */
+public class Player extends Sprite
+{
+    public Player(Sprite sprite)
+    {
+        super(sprite);
+    }
+}
