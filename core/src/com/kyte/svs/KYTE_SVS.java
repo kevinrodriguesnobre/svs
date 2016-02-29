d178f0fab048ced34ec9515a4b3e3206a22a31c1
kevinrodriguesnobre <4krodrig@vsispool11.informatik.uni-hamburg.de>
2016-02-29 13:30:06 +0100

    viewport

--- core/src/com/kyte/svs/KYTE_SVS.java
+++ core/src/com/kyte/svs/KYTE_SVS.java
@@ -8,14 +8,18 @@ import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 
@@ -25,7 +29,6 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 public class KYTE_SVS implements ApplicationListener {
 
     private OrthographicCamera camera;
-    private Viewport viewport;
     private Stage stage;
     private SpriteBatch batch;
     private Touchpad touchpad;
@@ -37,6 +40,11 @@ public class KYTE_SVS implements ApplicationListener {
     private Texture blockTexture;
     private Sprite blockSprite;
     private float blockSpeed;
+    private Rectangle viewport;
+    private static final int VIRTUAL_WIDTH = 480;
+    private static final int VIRTUAL_HEIGHT = 320;
+    private static final float ASPECT_RATIO =
+            (float)VIRTUAL_WIDTH/(float)VIRTUAL_HEIGHT;
 
     @Override
     public void create() {
@@ -48,7 +56,7 @@ public class KYTE_SVS implements ApplicationListener {
         camera.setToOrtho(false, 10f * aspectRatio, 10f); */
         camera = new OrthographicCamera();
         //viewport = new FitViewport(800, 480, camera);
-        viewport = new ScalingViewport(Scaling.fill, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
+        //viewport = new ScalingViewport(Scaling.fill, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
 
         //Create a touchpad skin
         touchpadSkin = new Skin();
@@ -70,9 +78,10 @@ public class KYTE_SVS implements ApplicationListener {
         touchpad.setBounds(15, 15, 100, 100);
 
         touchpad2 = new Touchpad(0, touchpadStyle);
-        touchpad2.setBounds(viewport.getWorldWidth()-viewport.getWorldHeight()/3, 15, viewport.getWorldHeight()/3, viewport.getWorldHeight()/3);
+        touchpad2.setBounds(480, 15, 100, 100);
         //Create a Stage and add TouchPad
         stage = new Stage();
+        //stage.setViewport(new ExtendViewport(800, 800));
         stage.addActor(touchpad);
         stage.addActor(touchpad2);
 
@@ -99,6 +108,9 @@ public class KYTE_SVS implements ApplicationListener {
         Gdx.gl.glClearColor(0.294f, 0.294f, 0.294f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         camera.update();
+        // set viewport
+        Gdx.gl.glViewport((int) viewport.x, (int) viewport.y,
+                (int) viewport.width, (int) viewport.height);
 
         //Move blockSprite with TouchPad
         double d = Math.atan2(touchpad.getKnobY(), touchpad.getKnobX());
@@ -109,22 +121,22 @@ public class KYTE_SVS implements ApplicationListener {
         blockSprite.setY(blockSprite.getY() + touchpad.getKnobPercentY() * blockSpeed);
 
         float rotation = blockSprite.getRotation();
-        if(touchpad2.getKnobPercentX() > 0){
-            if(touchpad2.getKnobPercentY() > 0){
-                rotation =(float) (-Math.atan(touchpad2.getKnobPercentX() / touchpad2.getKnobPercentY()) * 180 / Math.PI);
-            }else if(touchpad2.getKnobPercentY() < 0){
-                rotation =(float) -(-180 + Math.atan(touchpad2.getKnobPercentX() / touchpad2.getKnobPercentY()) * 180 / Math.PI);
+        if (touchpad2.getKnobPercentX() > 0) {
+            if (touchpad2.getKnobPercentY() > 0) {
+                rotation = (float) (-Math.atan(touchpad2.getKnobPercentX() / touchpad2.getKnobPercentY()) * 180 / Math.PI);
+            } else if (touchpad2.getKnobPercentY() < 0) {
+                rotation = (float) -(-180 + Math.atan(touchpad2.getKnobPercentX() / touchpad2.getKnobPercentY()) * 180 / Math.PI);
             }
-        }else if(touchpad2.getKnobPercentX() < 0){
-            if(touchpad2.getKnobPercentY() > 0){
-                rotation =(float) -(Math.atan(touchpad2.getKnobPercentX() / touchpad2.getKnobPercentY()) * 180 / Math.PI);
-            }else if(touchpad2.getKnobPercentY() < 0){
-                rotation =(float) -(-180 + Math.atan(touchpad2.getKnobPercentX() / touchpad2.getKnobPercentY()) * 180 / Math.PI);
+        } else if (touchpad2.getKnobPercentX() < 0) {
+            if (touchpad2.getKnobPercentY() > 0) {
+                rotation = (float) -(Math.atan(touchpad2.getKnobPercentX() / touchpad2.getKnobPercentY()) * 180 / Math.PI);
+            } else if (touchpad2.getKnobPercentY() < 0) {
+                rotation = (float) -(-180 + Math.atan(touchpad2.getKnobPercentX() / touchpad2.getKnobPercentY()) * 180 / Math.PI);
 
             }
         }
         blockSprite.setRotation(rotation);
-        
+
         //Draw
         batch.begin();
         blockSprite.draw(batch);
@@ -137,7 +149,29 @@ public class KYTE_SVS implements ApplicationListener {
 
     @Override
     public void resize(int width, int height) {
+        // calculate new viewport
+        float aspectRatio = (float) width / (float) height;
+        float scale = 1f;
+        Vector2 crop = new Vector2(0f, 0f);
 
+        if(aspectRatio > ASPECT_RATIO)
+        {
+            scale = (float)height/(float)VIRTUAL_HEIGHT;
+            crop.x = (width - VIRTUAL_WIDTH*scale)/2f;
+        }
+        else if(aspectRatio < ASPECT_RATIO)
+        {
+            scale = (float)width/(float)VIRTUAL_WIDTH;
+            crop.y = (height - VIRTUAL_HEIGHT*scale)/2f;
+        }
+        else
+        {
+            scale = (float)width/(float)VIRTUAL_WIDTH;
+        }
+
+        float w = (float)VIRTUAL_WIDTH*scale;
+        float h = (float)VIRTUAL_HEIGHT*scale;
+        viewport = new Rectangle(crop.x, crop.y, w, h);
     }
 
     @Override
