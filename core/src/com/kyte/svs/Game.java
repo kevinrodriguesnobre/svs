91b157272a25453eedac7ef652d60b03fe0d3a6c
kevinrodriguesnobre <4krodrig@vsispool11.informatik.uni-hamburg.de>
2016-03-01 13:28:24 +0100

    ShapeRender

--- core/src/com/kyte/svs/Game.java
+++ core/src/com/kyte/svs/Game.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
