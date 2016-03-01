eb5f35b4b78ba3b4cd6b381f545bc80092eae86f
kevinrodriguesnobre <4krodrig@vsispool11.informatik.uni-hamburg.de>
2016-03-01 13:27:55 +0100

    Camera follow

--- core/src/com/kyte/svs/Game.java
+++ core/src/com/kyte/svs/Game.java
@@ -22,13 +22,16 @@ public class Game extends ApplicationAdapter {
     private Joysticks _joysticks;
     private Player _player;
     private OrthographicCamera _camera;
-    // private SpriteBatch batch;
+    private SpriteBatch batch;
+    private ShapeRenderer sr;
     private static final int VIRTUAL_WIDTH = 480;
     private static final int VIRTUAL_HEIGHT = 320;
 
     @Override
     public void create()
     {
+        sr = new ShapeRenderer();
+        sr.setColor(0, 1, 0, 1);
         endTime = System.nanoTime();
         Texture texture = new Texture(Gdx.files.internal("data/Player.png"));
         Sprite _playerSprite = new Sprite(texture, 32, 32);
@@ -36,8 +39,8 @@ public class Game extends ApplicationAdapter {
         _player = new Player(_playerSprite);
         _player.setPosition(1, 1);
         _world = new World(_player, _camera);
-        //batch = new SpriteBatch();
-        //batch.getProjectionMatrix().setToOrtho2D(0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
+        batch = new SpriteBatch();
+        batch.getProjectionMatrix().setToOrtho2D(0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
         _joysticks = new Joysticks();
     }
 
@@ -49,17 +52,25 @@ public class Game extends ApplicationAdapter {
     @Override
     public void render()
     {
+        batch.setProjectionMatrix(_camera.combined);
+        sr.setProjectionMatrix(_camera.combined);
+
         float delta = System.nanoTime() - endTime;
-        _camera.update();
-        Gdx.gl.glClearColor(0, 0, 1, 0);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // Rotation des Spielers wird aktualisiert
         float tmpRot = _player.getRotation();
         _player.setRotation(_joysticks.getRotation(tmpRot));
 
-        // Position des Spielers wird aktualisiert
         _player.move(_joysticks.getPositionVector(), delta);
+        _camera.position.set(_player.getX(), _player.getY(), 1);
+        _camera.update();
+        sr.begin(ShapeRenderer.ShapeType.Filled);
+        //sr.circle(_player.getX(), _player.getY(), 30);
+        sr.circle(10, 10, 30);
+        sr.end();
+        System.out.println(_camera.position.x);
 
         // Zeichnen der grafischen Oberfläche
         _world.renderMap();
@@ -80,6 +91,6 @@ public class Game extends ApplicationAdapter {
 
     @Override
     public void dispose() {
-
+        batch.dispose();
     }
 }
