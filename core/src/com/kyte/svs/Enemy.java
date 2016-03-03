789c1089f112bd906678f2dccfa993cdf759ba62
kevinrodriguesnobre <4krodrig@informatik.uni-hamburg.de>
2016-03-03 13:33:01 +0100

    Enemy v0.2

--- core/src/com/kyte/svs/Enemy.java
+++ core/src/com/kyte/svs/Enemy.java
@@ -23,12 +23,14 @@ import java.util.ArrayList;
 public class Enemy extends Character {
     TiledMapTileLayer _collisionLayer;
     int _life;
+    int speed = 1;
 
 
     public Enemy() {
         super(new TextureRegion(new Texture("Gegner/Gegner.Alien.png"), 32, 32));
         setOriginY(16);
         setOriginX(16);
+        speed = speed + (int)Math.random();
     }
 
     public void move(Player target, ArrayList<Enemy> enemyList) {
@@ -59,10 +61,39 @@ public class Enemy extends Character {
             differenceY = -1;
         }
 
-        //differenceX = differenceX  * (newDeltaX / newDeltaY);
-        //differenceY = differenceY * (newDeltaY / newDeltaX);
+        if (newDeltaY != 0)
+            if ((newDeltaX / newDeltaY) < 1 && (newDeltaX / newDeltaY) > 0)
+                differenceX = differenceX * (newDeltaX / newDeltaY);
+        if (newDeltaX != 0)
+            if ((newDeltaY / newDeltaX) < 1 && (newDeltaY / newDeltaX) > 0)
+                differenceY = differenceY * (newDeltaY / newDeltaX);
+
         newX = getX() + differenceX;
         newY = getY() + differenceY;
+
+
+        //Rotierung muss am Anfang stehen
+        float rotation = getRotation();
+        if (differenceX > 0) {
+            if (differenceY > 0) {
+                rotation = (float) (-Math.atan(differenceX / differenceY) * 180 / Math.PI);
+            } else if (differenceY < 0) {
+                rotation = (float) -(-180 + Math.atan(differenceX / differenceY) * 180 / Math.PI);
+            }
+        } else if (differenceX < 0) {
+            if (differenceY > 0) {
+                rotation = (float) -(Math.atan(differenceX / differenceY) * 180 / Math.PI);
+            } else if (differenceY < 0) {
+                rotation = (float) -(-180 + Math.atan(differenceX / differenceY) * 180 / Math.PI);
+
+            }
+        }
+        /*if (rotation < getRotation() % 320 - getRotation() % 360 / 20 || rotation > getRotation() % 360 + getRotation() % 360 / 20) {
+            rotation = getRotation() + rotation / 20;
+        }*/
+
+        setRotation(rotation);
+
         // Wenn die Kachel, auf die der Spieler möchte ein blockiertes Objekt ist
         if (newX <= _collisionLayer.getWidth() * 32 && newY <= _collisionLayer.getHeight() * 32) {
             if (_collisionLayer.getCell((int) newX / _collisionLayer.getWidth(), (int) newY / _collisionLayer.getHeight()).getTile().getProperties().containsKey("blocked")) {
@@ -79,6 +110,7 @@ public class Enemy extends Character {
                         (Math.round(enemy.getY() + 30) > Math.round(getY()) && Math.round(enemy.getY() - 30) < Math.round(getY()))) {
                     //differenceX = -differenceX;
                     //differenceY = -differenceY;
+                    collided = true;
                     deltaX = Math.abs(enemy.getX()) - Math.abs(getX());
                     deltaY = Math.abs(enemy.getY()) - Math.abs(getY());
                     sum = Math.abs(deltaX + deltaY);
@@ -90,9 +122,8 @@ public class Enemy extends Character {
 
                         if (sum < 32)
                             return;
-                        differenceX = differenceX * -0.05f;
-                        differenceY = differenceY * -0.2f;
-                        collided = true;
+                        differenceX = differenceX * -0.6f * (float) Math.random();
+                        differenceY = differenceY * -0.6f * (float) Math.random();
                     }
                     //return;
                 }
@@ -100,43 +131,22 @@ public class Enemy extends Character {
         if (getX() + differenceX < 0 || getY() + differenceY < 0 || getX() + differenceX > _collisionLayer.getTileWidth() * 32 - 32 || getY() + differenceY > _collisionLayer.getTileHeight() * 32 - 32)
             return;
 
-        float rotation = getRotation();
-        if (differenceX > 0) {
-            if (differenceY > 0) {
-                rotation = (float) (-Math.atan(differenceX / differenceY) * 180 / Math.PI);
-            } else if (differenceY < 0) {
-                rotation = (float) -(-180 + Math.atan(differenceX / differenceY) * 180 / Math.PI);
-            }
-        } else if (differenceX < 0) {
-            if (differenceY > 0) {
-                rotation = (float) -(Math.atan(differenceX / differenceY) * 180 / Math.PI);
-            } else if (differenceY < 0) {
-                rotation = (float) -(-180 + Math.atan(differenceX / differenceY) * 180 / Math.PI);
 
-            }
-        }
-        if (rotation < getRotation() % 320 - getRotation() % 360 / 20 || rotation > getRotation() % 360 + getRotation() % 360 / 20) {
-            System.out.println(differenceX + " - " + differenceY);
-            rotation = getRotation() + rotation / 20;
-        }
-        if (!collided)
-            setRotation(rotation);
 
-        setX(getX() + differenceX);
-        setY(getY() + differenceY);
+
+        setX(getX() + differenceX * speed);
+        setY(getY() + differenceY * speed);
     }
 
     public void setCollisionLayer(TiledMapTileLayer collisionLayer) {
         _collisionLayer = collisionLayer;
     }
 
-    public void setLife(int newLife)
-    {
+    public void setLife(int newLife) {
         _life = newLife;
     }
 
-    public int getLife()
-    {
+    public int getLife() {
         return _life;
     }
 }
