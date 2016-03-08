9fa63378af0da806b11e006db614eb2de71d716c
kevinrodrigues95@hotmail.de <fhuorz9147>
2016-03-08 13:02:24 +0100

    sql

--- android/src/com/kyte/svs/AndroidLauncher.java
+++ android/src/com/kyte/svs/AndroidLauncher.java
@@ -2,15 +2,20 @@ package com.kyte.svs;
 
 import android.os.Bundle;
 
+import java.sql.*;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new START(), config);
-		System.out.print("Evgenij stinkt des todes #ih");
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new START(), config);
+    }
 }
